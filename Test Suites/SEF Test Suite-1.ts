5182ed52a95a2183492aa14cd4d6ef36ba5c3998
KanakaDwaraKanath <KanakaDwaraKanath@DWARAKANATHPC10.usc.ao.dcn>
2020-11-24 16:26:33 -0500

    SEF Test Suite Creation and Report configuration settings

---
+++ Test Suites/SEF Test Suite-1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SEF Test Suite-1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e1ae9455-edea-4b7b-98fc-db405d176ff1</testSuiteGuid>
+   <testCaseLink>
+      <guid>95e8e6b9-d7ec-4768-be7c-e218f4b07bb0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Login Page Test Case</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5f4a1d9c-7276-4423-a33c-86863f7035c9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Home Page Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
